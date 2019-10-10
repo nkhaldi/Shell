68ce9dd49311a271b33ad265db9994e3f1f935bf
Narek <mnk80898@gmail.com>
2019-10-10 19:08:53 +0300

    modified git.sh

--- git.sh
+++ git.sh
@@ -29,16 +29,14 @@ case $1 in
 	"-c" | "--commit")
 		if [ -z $2 ]; then
 			git add --all
-			echo "lol"
-		else
-			git add $2
-			echo "olo"
-		fi
-
-		if [ -z $3 ]; then
 			git commit -m "default commit messege"
 		else
-			git commit -m "$3"
+			git add $2
+			if [ -z $3 ]; then
+				git commit -m "default commit messege"
+			else
+				git commit -m "$3"
+			fi
 		fi
 		git push
 		perl -E 'say "\033[32m"'

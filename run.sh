8e3613af3ca98c8e2075c0259766e67f874ae698
Narek <mnk80898@gmail.com>
2019-10-31 01:56:22 +0300

    modified git and run

--- run.sh
+++ run.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-# alias run='sh ~/Scripts/run.sh'
 
 if [ -z $1 ]; then
 	./a.out
 	exit 0
 fi
+case $1 in
+	"c")
+		if [ -z $2 ]; then
+			echo "usage: compiler is not selected"
+			echo "use -h or --help to get help"
+		else
+			if [ -z $3 ]; then
+				perl -E 'say "\033[31m"'
+				echo "usage: no filename"
+				echo "use -h or --help to get help"
+			else
+				gcc -Wall -Werror -Wextra $3 -o main
+				./main
+			fi
+		fi
+	;;
+	"cpp")
+		if [ -z $2 ]; then
+			echo "usage: compiler is not selected"
+			echo "use -h or --help to get help"
+		else
+			if [ -z $3 ]; then
+				perl -e 'say "\033[31m"'
+				echo "usage: no filename"
+				echo "use -h or --help to get help"
+			else
+				gcc -Wall -Werror -Wextra $3 -o main
+				./main
+			fi
+		fi
+	;;
+esac
+perl -E 'say "\033[31m"'
+
+exit 0

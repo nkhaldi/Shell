1387f4e166edee68379d9d1117349fb5231e901f
Joker Krypto <jkrypto@mi-r2.21-school.ru>
2019-12-05 19:13:25 +0300

    default commit messege

--- git.sh
+++ git.sh
@@ -73,16 +73,11 @@ case $1 in
 		git status
 	;;
 	"-u" | "--ultimate")
+		git add --all
 		if [ -z $2 ]; then
-			git add --all
 			git commit -m "default commit messege"
 		else
-			git add $2
-			if [ -z $3 ]; then
-				git commit -m "default commit messege"
-			else
-				git commit -m "$3"
-			fi
+			git commit -m "$3"
 		fi
 		git push
 		perl -E 'say "\033[32m"'

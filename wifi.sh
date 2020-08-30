a219d0dce9bb89a98d17f30ab6274943fd51f70d
nkhaldi <mnk80898@gmail.com>
2020-08-30 21:57:23 +0300

    modificated scripts

--- wifi.sh
+++ wifi.sh
@@ -3,5 +3,5 @@
 cat ~/.password | sudo -S echo
 perl -E 'say "\033[35m*** RESTARTING WIFI ***\033[0m"'
 sudo modprobe -rv rtl8723be
-sudo modprobe -v rtl8723be ant_sel=2
+sudo modprobe -v rtl8723be ant_sel=1
 perl -E 'say "\033[32m*** WIFI RESTARTED ***\033[0m"'

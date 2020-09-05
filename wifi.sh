f8413034c110a0571b9cb10a9fc154e47e72a5bd
nkhaldi <mnk80898@gmail.com>
2020-09-05 14:09:09 +0300

    modified path

--- wifi.sh
+++ wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat ~/.password | sudo -S echo
+cat /home/narek/.password | sudo -S echo
 perl -E 'say "\033[35m*** RESTARTING WIFI ***\033[0m"'
 sudo modprobe -rv rtl8723be
 sudo modprobe -v rtl8723be ant_sel=1

82dd11cfdfb9d05686fafda3fc9e4cba2851523a
nkhaldi <mnk80898@gmail.com>
2021-02-16 14:25:03 +0300

    added option to wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -3,5 +3,11 @@
 cat /home/narek/.pass/.password | sudo -S echo
 perl -E 'say "\033[35m*** RESTARTING WIFI ***\033[0m"'
 sudo modprobe -rv rtl8723be
-sudo modprobe -v rtl8723be ant_sel=1
+
+if [ -z $1 ]; then
+    sudo modprobe -v rtl8723be ant_sel=1
+else
+    sudo modprobe -v rtl8723be ant_sel=$1
+fi
+
 perl -E 'say "\033[32m*** WIFI RESTARTED ***\033[0m"'

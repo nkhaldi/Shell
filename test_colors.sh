16d50ae39636ec18c91d2a7d30d5437cc1efbe41
nkhaldi <mnk80898@gmail.com>
2021-04-26 11:19:43 +0300

    added test_colors.sh

---
+++ test_colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+perl -e 'print "\033[30m[30]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[31m[31]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[32m[32]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[33m[33]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[34m[34]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[35m[35]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[36m[36]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
+perl -e 'print "\033[37m[37]|   1234567890  qwertyuiop  ~!@#$%^&*(\n"'
